0bfdac7bb3a232c5af38f17f0c1887d0fba33c64
Shina Takada <stkd1001@gmail.com>
2019-10-15 16:01:24 +0900

    2019/10/15

---
+++ picoCTF/GeneralSkills/Based.py
@@ -0,0 +1,51 @@
+from pwn import *
+
+def convert(enc_word, base):
+    dec_word = ""
+    for chr_bin in enc_word:
+        dec_word += chr(int(chr_bin, base))
+    return dec_word
+
+def solve_base_2(p):
+    p.readuntil("Please give the ")
+    enc_word = p.readuntil(" as").decode("utf-8").split(" ")[:-1]
+    log.info("enc_word: {}".format(enc_word))
+
+    dec_word = convert(enc_word, 2)
+    
+    log.info("dec_word: {}".format(dec_word))
+    p.sendlineafter("Input:", dec_word)
+
+def solve_base_8(p):
+    p.readuntil("Please give me the  ")
+    enc_word = p.readuntil(" as").decode("utf-8").split(" ")[:-1]
+    log.info("enc_word: {}".format(enc_word))
+
+    dec_word = convert(enc_word, 8)
+    
+    log.info("dec_word: {}".format(dec_word))
+    p.sendlineafter("Input:", dec_word)
+
+def solve_base_16(p):
+    p.readuntil("Please give me the ")
+    s = p.readuntil(" as").decode("utf-8").split(" ")[0]
+    enc_word = [s[i: i+2] for i in range(0, len(s), 2)]
+    log.info("enc_word: {}".format(enc_word))
+
+    dec_word = convert(enc_word, 16)
+    
+    log.info("dec_word: {}".format(dec_word))
+    p.sendlineafter("Input:", dec_word)
+
+def main():
+    context(arch="i386", os="linux")
+
+    p = remote("2019shell1.picoctf.com", 31615)
+
+    solve_base_2(p)
+    solve_base_8(p)
+    solve_base_16(p)
+    p.interactive()
+
+if __name__ == "__main__":
+    main()
